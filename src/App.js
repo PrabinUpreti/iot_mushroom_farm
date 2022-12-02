e7cbd6332bb8af4f298e0ac0ec8af50e8772fe9f
Prabin Upreti <upretirajprabin@gmail.com>
2022-12-02 10:25:43 +0545

    Some changes or flushed garbage files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Line } from "react-chartjs-2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1>Welcome to IoT Mushroom Farm</h1>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col">2</div>
+        {/* <div className="col">3</div>
+        <div className="col">4</div> */}
+      </div>
     </div>
   );
 }
